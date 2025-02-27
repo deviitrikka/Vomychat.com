c88672563fea058bff1bd07cf9c8623e54e3a068
deviitrikka <aitrika09@gmail.com>
2025-02-27 23:21:51 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,10 @@
 import { Suspense } from "react";
-import NavbarContent from "./NavbarContent"; // Separate the component logic
+import NavbarComponent from "./NavbarComponent"; // Separate the component logic
 
 export default function Navbar() {
     return (
         <Suspense fallback={<p>Loading...</p>}>
-            <NavbarContent />
+            <NavbarComponent />
         </Suspense>
     );
 }
