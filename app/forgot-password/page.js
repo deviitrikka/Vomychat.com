6280da2bf6f0678b24b2d23f934fb51cefee00b3
deviitrikka <aitrika09@gmail.com>
2025-03-01 13:58:08 +0530

    Update page.js

--- app/forgot-password/page.js
+++ app/forgot-password/page.js
@@ -17,7 +17,7 @@ const ForgotPassword = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/forgot-password", {
+      const response = await fetch("https://linktreeclone-backend-production-ad40.up.railway.app/api/auth/forgot-password", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email: emailOrUsername }),
