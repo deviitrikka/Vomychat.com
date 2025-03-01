9424b1a35a7707f1f99484950d88ba5b73f3d1c8
deviitrikka <aitrika09@gmail.com>
2025-03-01 13:58:48 +0530

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -24,7 +24,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://linktreeclone-backend-production-ad40.up.railway.app/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
